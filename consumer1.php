4666763443939161b6a00e83efd189b32ec0e3e3
lq <liuqianglan@sina.com>
2021-03-19 14:33:37 +0800

    dasf

--- consumer1.php
+++ consumer1.php
@@ -3,36 +3,33 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Exchange\AMQPExchangeType;
 
-$routeKeys = $argv[1] ? array_slice($argv, 1) : ['info', 'error', 'warn'];
+/**
+ * 订阅消息
+ */
 
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
 //声明初始化交换机
 //参数：交换机名，路由类型，是否检测同名队列，是否开启队列持久化，通道关闭后是否删除队列
-$channel->exchange_declare('mq_sms_send_ex5', AMQPExchangeType::DIRECT, false, false, false);
+$channel->exchange_declare('mq_sms_send_ex4', AMQPExchangeType::FANOUT, false, false, false);
 
 //声明初始化一条队列
 //参数：队列名，是否检测同名队列，是否开启队列持久化，是否能被其他队列访问，通道关闭后是否删除队列
-list($queueName, ,) = $channel->queue_declare('', false, false, false, false);
+list($queue_name, ,) = $channel->queue_declare('', false, false, true, false);
 
 //将队列与某个交换机进行绑定，并使用路由关键字
 //参数：队列名，交换机名，路由键名
-foreach ($routeKeys as $k => $v) {
-    $channel->queue_bind($queueName, 'mq_sms_send_ex5', $v);
-}
+$channel->queue_bind($queue_name, 'mq_sms_send_ex4');
 
 echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
 
 $callback = function($msg) {
     echo " [x] Received ", $msg->body, "\n";
 
-    //消息确认
-//    $msg->ack();
-//    echo 1111;
-
     //判断获取到quit后
     if (trim($msg->body) == 'quit') {
+
         $msg->getChannel()->basic_cancel($msg->getConsumerTag());
     }
 
@@ -42,7 +39,7 @@ $channel->basic_qos(null, 1, null);
 
 //
 //参数：队列名，消费者标识符，不接收此使用者发布的消息，使用者是否使用自动确认模式，请求独占使用者访问，不等待，消息回调函数
-$channel->basic_consume($queueName, 'consumer1', false, true, false, false, $callback);
+$channel->basic_consume($queue_name, 'consumer1', false, true, false, false, $callback);
 
 
 function shutdown($channel, $connection)
--- producer1.php
+++ producer1.php
@@ -1,7 +1,6 @@
 <?php
-
 /**
- * direct交换机
+ * fanout 交换机
  */
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
@@ -9,10 +8,8 @@ use PhpAmqpLib\Message\AMQPMessage;
 use PhpAmqpLib\Exchange\AMQPExchangeType;
 
 /**
- * $argv  传递给脚本的参数数组
- * $argc  传递给脚本的参数数目
+ * 发布消息与订阅
  */
-$routeKey = $argv[1] ?? 'info';
 
 //获取终端提示用户输入数据
 //php cli中，三个系统常量：STDIN STDOUT STDERR 代表文件句柄
@@ -28,15 +25,8 @@ $channel = $connection->channel();
 
 //初始化交换机
 //参数：交换机名，路由类型，是否检测同名队列，是否开启队列持久化，通道关闭后是否删除队列
-$channel->exchange_declare('mq_sms_send_ex5', AMQPExchangeType::DIRECT, false, false, false);
+$channel->exchange_declare('mq_sms_send_ex4', AMQPExchangeType::FANOUT, false, false, false);
 
-//声明初始化一条队列
-//参数：队列名，是否检测同名队列，是否开启队列持久化，是否能被其他队列访问，通道关闭后是否删除队列
-//$channel->queue_declare('mq_sms_send_q', false, true, false, false);
-
-//将队列与某个交换机进行绑定，并使用路由关键字
-//参数：队列名，交换机名，路由键名
-//$channel->queue_bind('mq_sms_send_q1', 'mq_sms_send_ex', 'sms_send');
 
 //生成消息
 //消息持久化 设置delivery_mode为2
@@ -44,7 +34,7 @@ $msg = new AMQPMessage($msg_str);
 
 //推送消息到某个交换机
 //参数：消息，交换机，路由键名
-$channel->basic_publish($msg, 'mq_sms_send_ex5', $routeKey);
+$channel->basic_publish($msg, 'mq_sms_send_ex4');
 echo ' [X] Sent: ' . $msg_str . "\n";
 
 $channel->close();
